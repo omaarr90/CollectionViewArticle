6c722d5e74665263a6203fbeb56ae1edc43306d3
Omar Alshammari <omar.alshammary@hotmail.com>
2018-07-22 23:37:02 +0300

    Initial Commit

---
+++ CollectionViewDemoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionViewDemoApp
+//
+//  Created by Omar Alshammari on 09/11/1439 AH.
+//  Copyright © 1439 Omar Alshammari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
